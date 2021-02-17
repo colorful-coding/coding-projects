6760d94188271205b1073b1b5f90e01edd269f6f
Colorful Coding <73709886+colorful-coding@users.noreply.github.com>
2021-02-17 09:45:57 +0100

    Add files via upload

---
+++ Coding Project #16/script.js
@@ -0,0 +1,31 @@
+function setup() {
+    createCanvas(500, 500)
+    angleMode(DEGREES)
+}
+
+function draw() {
+    background(20)
+    stroke(255)
+    strokeWeight(5)
+    noFill()
+
+    translate(width / 2, height / 2)
+
+    beginShape()
+    for (var i = 0; i < 359; i++) {
+
+        var r1Min = map(sin(frameCount), -1, 1, 50, 120)
+        var r1Max = map(sin(frameCount * 2), -1, 1, 100, 20)
+
+        var r2Min = map(sin(frameCount / 2), -1, 1, 120, 50)
+        var r2Max = map(sin(frameCount), -1, 1, 20, 100)
+
+        var r1 = map(sin(i * 3), -1, 1, r1Min, r1Max)
+        var r2 = map(sin(i * 6 + 90), -1, 1, r2Min, r2Max)
+        var r = r1 + r2
+        var x = r * cos(i)
+        var y = r * sin(i)
+        vertex(x, y)
+    }
+    endShape(CLOSE)
+}
